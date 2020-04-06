c734b38868f7e43d14910c56c9def250fb854bc1
Justin <brutalgg@users.noreply.github.com>
2020-04-05 21:56:47 -0400

    implement flag logic and dns query logic

--- cmd/root.go
+++ cmd/root.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"errors"
 	"fmt"
-	"net"
 	"time"
 
 	dga "github.com/brutalgg/gobermann/internal/domaingeneratingalgorithm"
@@ -11,12 +10,13 @@ import (
 	"github.com/brutalgg/gobermann/plugins/dga/locky"
 	"github.com/brutalgg/gobermann/plugins/dga/necurs"
 	"github.com/brutalgg/gobermann/plugins/dga/nymaim2"
+	"github.com/miekg/dns"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
 	Use:              "gobermann",
-	Version:          "",
+	Version:          "1.0alpha",
 	PersistentPreRun: setup,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
@@ -30,10 +30,10 @@ func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
-	rootCmd.PersistentFlags().BoolP("verbose", "v", false, "Include verbose messages from program execution")
-	rootCmd.PersistentFlags().BoolP("dryrun", "r", true, "Determines whether DNS traffic is sent over the wire")
+	rootCmd.PersistentFlags().StringP("loglevel", "l", "info", "Include verbose messages from program execution [error, warn, info, debug]")
+	rootCmd.PersistentFlags().BoolP("dryrun", "r", true, "When enabled dns traffic will not be sent over the wire")
 	rootCmd.PersistentFlags().IntP("burst", "b", 15, "Number of requests in a burst of DNS traffic")
-	rootCmd.PersistentFlags().IntP("delay", "d", 1, "Delay between requets in a burst in seconds")
+	rootCmd.PersistentFlags().IntP("delay", "d", 500, "Delay between requets in a burst in milliseconds")
 	rootCmd.PersistentFlags().IntP("interval", "i", 720, "Delay between bursts in minutes")
 	rootCmd.PersistentFlags().StringP("dns", "s", "1.1.1.1", "Target DNS Server")
 	rootCmd.PersistentFlags().StringP("alg", "a", "locky", "The domain generating algorithm to use.[locky, nymaim2, necurs]")
@@ -48,45 +48,66 @@ func Execute() {
 }
 
 func setup(cmd *cobra.Command, args []string) {
-	alg, _ := cmd.Flags().GetString("alg")
-	dns, _ := cmd.Flags().GetString("dns")
+	l, _ := cmd.Flags().GetString("loglevel")
+	switch l {
+	case "error":
+		cli.SetPrintLevel(3)
+	case "warn":
+		cli.SetPrintLevel(2)
+	case "debug":
+		cli.SetPrintLevel(0)
+	default:
+		cli.SetPrintLevel(1)
+	}
 
+	alg, _ := cmd.Flags().GetString("alg")
 	if _, err := selectDGA(alg); err != nil {
 		cli.Fatal("%v is not one of the supported algorithms", alg)
 	}
-	// trying to figure out if this test could be better
-	cli.Info("Testing Connection to %v", dns)
-	if _, err := net.Dial("tcp", fmt.Sprintf("%v:53", dns)); err != nil {
-		cli.Fatal("could not connect to server %v", dns)
-	}
 
+	if cmd.Flags().Changed("dryrun") {
+		dns, _ := cmd.Flags().GetString("dns")
+		cli.Debug("Testing Connection to %v", dns)
+		if err := dnsQuery("no-reply-kt.com", dns); err != nil {
+			cli.Fatalln(err)
+		}
+	} else {
+		cli.Debugln("dryrun detected: no dns requets will be sent")
+	}
 }
 
 func run(cmd *cobra.Command, args []string) {
-	// todo implement other flags
 	alg, _ := cmd.Flags().GetString("alg")
+	interval, _ := cmd.Flags().GetInt("interval")
+
 	for {
-		dga, ok := selectDGA(alg)
-		if ok != nil {
-			cli.Fatal("Empty DGA detected. How did you even hit this message?")
+		dga, err := selectDGA(alg)
+		if err != nil {
+			cli.Fatalln("Empty DGA detected. How did you even hit this message?")
 		}
-		burst(25, 0, dga)
-		cli.Info("completed burst")
-		time.Sleep(time.Minute)
+		burst(cmd, dga)
+		cli.Infoln("Waiting for interval...")
+		time.Sleep(time.Minute * time.Duration(interval))
 	}
 }
 
-func burst(burst int, delay int, d dga.DomainGenerator) {
-	// todo implement sending of messages
-	// var msg dns.Msg
-	// fqdn := dns.Fqdn("google.com")
-	// msg.SetQuestion(fqdn, dns.TypeA)
-	// dns.Exchange(&msg, "8.8.8.8:53")
+func burst(cmd *cobra.Command, d dga.DomainGenerator) {
+	burst, _ := cmd.Flags().GetInt("burst")
+	delay, _ := cmd.Flags().GetInt("delay")
+	dns, _ := cmd.Flags().GetString("dns")
+
+	cli.Infoln("Starting Burst")
 	for i := 0; i < burst; i++ {
-		cli.Info(d.GenerateDomain())
-		time.Sleep(time.Second * time.Duration(delay))
+		domain := d.GenerateDomain()
+		cli.Debugln(domain)
+		if cmd.Flags().Changed("dryrun") {
+			dnsQuery(domain, dns)
+		}
+		time.Sleep(time.Millisecond * time.Duration(delay))
 	}
+	cli.Infoln("Burst Completed Successfully")
 }
+
 func selectDGA(x string) (dga.DomainGenerator, error) {
 	switch x {
 	case "locky":
@@ -98,4 +119,14 @@ func selectDGA(x string) (dga.DomainGenerator, error) {
 	}
 
 	return dga.DefaultGenerator{}, errors.New("using empty generator")
+}
+
+func dnsQuery(f string, server string) error {
+	var msg dns.Msg
+	fqdn := dns.Fqdn(f)
+	msg.SetQuestion(fqdn, dns.TypeA)
+	if _, err := dns.Exchange(&msg, fmt.Sprintf("%v:53", server)); err != nil {
+		return err
+	}
+	return nil
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/brutalgg/gobermann
 go 1.13
 
 require (
-	github.com/miekg/dns v1.1.29 // indirect
+	github.com/miekg/dns v1.1.29
 	github.com/spf13/cobra v0.0.7
 	honnef.co/go/tools v0.0.1-2020.1.3 // indirect
 )
